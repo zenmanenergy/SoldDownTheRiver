ed6558026748da9806eca090fe7bc3acad08ed90
Steve Nelson <snelson@zenmanenergy.org>
2023-04-16 17:37:42 -0400

    initial commit

---
+++ Client/src/routes/User/handleDelete.js
@@ -0,0 +1,16 @@
+// src/routes/Users/handleDelete.js
+const baseURL = "http://192.168.1.182";
+export async function handleDelete(UserId) {
+	try {
+		const response = await fetch(`${baseURL}/DeleteUser?UserId=${UserId}`);
+
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+
+		console.log(`User with ID ${UserId} has been deleted.`);
+		window.location.href = "/Users";
+	} catch (error) {
+		console.error("There was a problem deleting the user:", error);
+	}
+}
