2b95dad003bd94642bab236a57f50f4198c98284
Steve Nelson <snelson@zenmanenergy.org>
2023-04-16 16:00:50 -0400

    initial commit

---
+++ Client/src/routes/Location/handleSave.js
@@ -0,0 +1,42 @@
+// src/routes/Locations/handleSave.js
+const baseURL = 'http://192.168.1.182';
+// src/routes/Locations/handleSave.js
+export function handleSave(locationId, city, state, country, latitude, longitude, formValid) {
+
+  if (!formValid) {
+		const invalidFields = document.querySelectorAll("input:invalid");
+		if (invalidFields.length > 0) {
+			invalidFields[0].focus();
+		}
+		return;
+	}
+
+  const locationData = {
+    LocationId: locationId,
+    City: city,
+    State: state,
+    Country: country,
+    Latitude: latitude,
+    Longitude: longitude
+  };
+
+  const queryString = Object.keys(locationData)
+    .map(key => key + '=' + encodeURIComponent(locationData[key]))
+    .join('&');
+
+  const url = baseURL + '/SaveLocation?' + queryString; 
+  console.log(url)
+  fetch(url, {
+    method: 'GET'
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log("Save success",data);
+    window.location.href = '/Locations';
+    // Handle the response data as needed
+  })
+  .catch(error => {
+    console.error("save error",error);
+    // Handle the error as needed
+  });
+}
