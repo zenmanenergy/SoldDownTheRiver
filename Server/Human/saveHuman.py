407cd91f473d64164715b05735b9bc4f3080658f
Steve Nelson <snelson@zenmanenergy.org>
2023-04-16 15:22:58 -0400

    added new fields

--- Server/Human/saveHuman.py
+++ Server/Human/saveHuman.py
@@ -1,20 +1,20 @@
 import uuid
 from Lib import Database
 
-def save_human(HumanId, FirstName, MiddleName, LastName):
+def save_human(HumanId, FirstName, MiddleName, LastName, StartYear, EndYear, Notes):
     # Connect to the database
     cursor, connection = Database.ConnectToDatabase()
 
     # Check if the HumanId is present
     if HumanId:
         # If the HumanId is present, update the existing human
-        query = "UPDATE Humans SET FirstName = %s, MiddleName = %s, LastName = %s WHERE HumanId = %s"
-        values = (FirstName, MiddleName, LastName, HumanId)
+        query = "UPDATE Humans SET FirstName = %s, MiddleName = %s, LastName = %s, StartYear = %s, EndYear = %s, Notes = %s WHERE HumanId = %s"
+        values = (FirstName, MiddleName, LastName, StartYear, EndYear, Notes, HumanId)
     else:
         # If the HumanId is not present, create a new human
         HumanId = str(uuid.uuid4())
-        query = "INSERT INTO Humans (HumanId, FirstName, MiddleName, LastName) VALUES (%s, %s, %s, %s)"
-        values = (HumanId, FirstName, MiddleName, LastName)
+        query = "INSERT INTO Humans (HumanId, FirstName, MiddleName, LastName, StartYear, EndYear, Notes) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        values = (HumanId, FirstName, MiddleName, LastName, StartYear, EndYear, Notes)
 
     # Execute the query and commit the changes
     cursor.execute(query, values)
