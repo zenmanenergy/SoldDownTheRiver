59bb88e4374c1d4665548f64c7ab534a6a413aee
Steve Nelson <snelson@zenmanenergy.org>
2023-04-16 17:38:20 -0400

    initial commit

---
+++ Server/User/SaveUser.py
@@ -0,0 +1,28 @@
+import uuid
+from Lib import Database
+
+def save_user(UserId, FirstName, LastName, Email, Phone, Password, School, SemesterYear):
+    # Connect to the database
+    cursor, connection = Database.ConnectToDatabase()
+
+    # Check if the UserId is present
+    if UserId:
+        # If the UserId is present, update the existing user
+        query = "UPDATE Users SET FirstName = %s, LastName = %s, Email = %s, Phone = %s,Password=%s, School = %s, SemesterYear = %s WHERE UserId = %s"
+        values = (FirstName, LastName, Email, Phone, Password, School, SemesterYear, UserId)
+    else:
+        # If the UserId is not present, create a new user
+        UserId = str(uuid.uuid4())
+        query = "INSERT INTO Users (UserId, FirstName, LastName, Email, Phone, Password, School, SemesterYear) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        values = (UserId, FirstName, LastName, Email, Phone, Password,School, SemesterYear)
+
+    # Execute the query and commit the changes
+    print(query, values)
+    cursor.execute(query, values)
+    connection.commit()
+
+    # Close the database connection
+    connection.close()
+
+    # Return the UserId as a JSON response
+    return {'success': True, 'UserId': UserId}
