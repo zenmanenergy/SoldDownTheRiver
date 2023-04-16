d2f2b9fe914290ecca70b728ca69e3795936e9e4
Steve Nelson <snelson@zenmanenergy.org>
2023-04-16 16:01:20 -0400

    initial commit

---
+++ Server/Location/Location.py
@@ -0,0 +1,52 @@
+from Lib import Database
+from flask import Blueprint, request
+from flask_cors import CORS, cross_origin
+from .SaveLocation import save_location
+from .DeleteLocation import delete_location
+from .GetLocation import get_location
+
+blueprint = Blueprint('Location', __name__)
+
+@blueprint.route("/SaveLocation", methods=['GET'])
+@cross_origin()
+def SaveLocation():
+    location_data = request.args.to_dict()
+
+    # Extract the location information from the location_data
+    LocationId = location_data.get('LocationId', None)
+    City = location_data.get('City', None)
+    State = location_data.get('State', None)
+    Country = location_data.get('Country', None)
+    Latitude = location_data.get('Latitude', None)
+    Longitude = location_data.get('Longitude', None)
+
+    # Call the save_location function from SaveLocation.py with the extracted data
+    result = save_location(LocationId, City, State, Country, Latitude, Longitude)
+
+    return result
+    
+    
+
+@blueprint.route("/DeleteLocation", methods=['GET'])
+@cross_origin()
+def DeleteLocation():
+    # Get the location information from the request
+    location_data = request.args.to_dict()
+
+    # Get the location ID from the request
+    LocationId = location_data.get('LocationId')
+    # Call the delete_location function from DeleteLocation.py
+    result = delete_location(LocationId)
+    return result
+
+@blueprint.route("/GetLocation", methods=['GET'])
+@cross_origin()
+def GetLocation():
+    # Get the location information from the request
+    location_data = request.args.to_dict()
+
+    # Get the location ID from the request
+    LocationId = location_data.get('LocationId')
+    # Call the get_location function from GetLocation.py
+    result = get_location(LocationId)
+    return result
