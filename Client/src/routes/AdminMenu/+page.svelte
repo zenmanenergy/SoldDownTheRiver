ee3451708eab54529a3469495b7bb1ac6d1898cc
Steve Nelson <snelson@zenmanenergy.org>
2023-04-16 17:37:28 -0400

    initial commit

---
+++ Client/src/routes/AdminMenu/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { onMount } from "svelte";
+
+	let isLoading = true;
+
+	onMount(async () => {
+		isLoading = false;
+	});
+</script>
+
+{#if isLoading}
+	<div class="loading-screen">
+		<div class="spinner" />
+	</div>
+{:else}
+	<div>
+		<h1>Admin Menu</h1>
+		<ul>
+			<li><a href={`/Businesses`}>Businesses</a></li>
+			<li><a href={`/Humans`}>Humans</a></li>
+			<li><a href={`/Locations`}>Locations</a></li>
+			<li><a href={`/Users`}>Users</a></li>
+		</ul>
+	</div>
+{/if}
