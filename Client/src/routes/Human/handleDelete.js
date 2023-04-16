442fcdb00b20b5dca9991a6c28c5d3c59f925c94
Steve Nelson <snelson@zenmanenergy.org>
2023-04-16 15:21:49 -0400

    Update handleDelete.js
    added  fetch

--- Client/src/routes/Human/handleDelete.js
+++ Client/src/routes/Human/handleDelete.js
@@ -1,6 +1,16 @@
-// src/routes/Humans/handleDelete.js
-export function handleDelete(HumanId) {
-    console.log('Delete:', { HumanId });
-    // Implement the delete logic here
+const baseURL = 'http://192.168.1.182';
+
+export async function handleDelete(HumanId) {
+  try {
+    const response = await fetch(`${baseURL}/DeleteHuman?HumanId=${HumanId}`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    console.log(`Human with ID ${HumanId} has been deleted.`);
+    window.location.href = '/Humans';
+  } catch (error) {
+    console.error('There was a problem deleting the human:', error);
   }
-  
+}
