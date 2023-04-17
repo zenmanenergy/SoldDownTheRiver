e97cb718ddc3b37a917664e885bf10b22b51b515
Steve Nelson <snelson@zenmanenergy.org>
2023-04-17 15:41:02 -0400

    css
    Added css box around the content
    Added form field classes

--- Client/src/routes/Business/+page.svelte
+++ Client/src/routes/Business/+page.svelte
@@ -1,69 +1,77 @@
 <!-- src/routes/Businesses/+page.svelte -->
+<style>
+	@import '/static/FormPages.css';
+  </style>
 <script>
 	import { onMount } from 'svelte';
 	import { handleSave } from './handleSave.js';
 	import { handleDelete } from './handleDelete.js';
 	import { handleGet } from './handleGet.js';
-
+  
 	let BusinessId="";
 	let BusinessName = "";
 	let formValid = false;
-  	let isLoading = true;
-
+	let isLoading = true;
+  
 	async function setName(newBusinessName) {
-		BusinessName = newBusinessName;
+	  BusinessName = newBusinessName;
 	}
-
+  
 	$: {
-		formValid = BusinessName;
+	  formValid = BusinessName;
 	}
+  
 	onMount(async () => {
-		const urlParams = new URLSearchParams(window.location.search);
-		BusinessId = urlParams.get("BusinessId") || "";
-		if (BusinessId) {
-			handleGet(BusinessId, setName);
-		}
-		console.log("BusinessId", BusinessId)
-    	isLoading = false;
+	  const urlParams = new URLSearchParams(window.location.search);
+	  BusinessId = urlParams.get("BusinessId") || "";
+	  if (BusinessId) {
+		handleGet(BusinessId, setName);
+	  }
+	  console.log("BusinessId", BusinessId)
+	  isLoading = false;
 	});
-</script>
-
-<style>
-	input:required:invalid {
-		border: 1px solid red;
-	}
-	input:required:valid {
-		border: 1px solid #ccc;
-	}
-</style>
-{#if isLoading}
-  <div class="loading-screen">
-    <div class="spinner"></div>
-  </div>
-{:else}
-	<div>
-		<form>
-			<h2>Add a Business</h2>
-
-			<input type="hidden" bind:value={BusinessId} />
-
-			<div>
-				<label for="BusinessName">Name</label>
-				<input
-					type="text"
-					id="BusinessName"
-					placeholder="Enter Business Name"
-					bind:value={BusinessName}
-					required
-				/>
-			</div>
-
-			<div>
-				<button type="button" on:click={() => handleSave(BusinessId, BusinessName,formValid)} >Save</button>
-				{#if BusinessId.length}
-					<button type="button" on:click={() => handleDelete(BusinessId)}>Delete</button>
-				{/if}
-			</div>
-		</form>
+  </script>
+  
+  
+  
+  {#if isLoading}
+	<div class="loading-screen">
+	  <div class="spinner"></div>
 	</div>
-{/if}
+  {:else}
+  
+	<div class="section">
+		<a href="/Businesses">Back to Businesses</a>
+		<div class="ActionBox">
+	  <form>
+		<h3 class="title is-2">Add a Business</h3>
+  
+		<input type="hidden" bind:value={BusinessId} />
+  
+		<div class="field">
+		  <label class="label" for="BusinessName">Name</label>
+		  <div class="control">
+			<input
+			  class="input"
+			  type="text"
+			  id="BusinessName"
+			  placeholder="Enter Business Name"
+			  bind:value={BusinessName}
+			  required
+			/>
+		  </div>
+		</div>
+  
+		<div class="field">
+		  <div class="control">
+			<button class="button is-primary" type="button" on:click={() => handleSave(BusinessId, BusinessName,formValid)}>Save</button>
+			{#if BusinessId.length}
+			  <button class="button is-danger" type="button" on:click={() => handleDelete(BusinessId)}>Delete</button>
+			{/if}
+		  </div>
+		</div>
+	  </form>
+	</div>
+	</div>
+  {/if}
+  
