37c2e87bdba041022a52ccd270fb60a77a9ccf65
Steve Nelson <snelson@zenmanenergy.org>
2023-04-19 14:36:13 -0400

    initial commit

---
+++ Server/Human/GetAKANames.py
@@ -0,0 +1,24 @@
+from Lib import Database
+
+def get_aka(HumanId):
+    if not HumanId:
+        HumanId="-1"
+    
+    # Connect to the database
+    cursor, connection = Database.ConnectToDatabase()
+
+    # Construct the SQL query
+    query = "SELECT * FROM HumansAKA WHERE HumanId = %s"
+    values = (HumanId,)
+    print(query, values)
+    # Execute the query and get the results
+    cursor.execute(query, values)
+    result = cursor.fetchall()
+    if not result:
+        result={}
+        
+    # Close the database connection
+    connection.close()
+    
+    # Return the result as a dictionary
+    return result
