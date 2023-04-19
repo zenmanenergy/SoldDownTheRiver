4a927464d2c2087b13ddfd2190e93405d8199600
Steve Nelson <snelson@zenmanenergy.org>
2023-04-19 14:35:27 -0400

    Add UI for get, save and delete AKA names

--- Client/src/routes/Human/+page.svelte
+++ Client/src/routes/Human/+page.svelte
@@ -8,7 +8,10 @@
   import { handleSave } from './handleSave.js';
   import { handleDelete } from './handleDelete.js';
   import { handleGet } from './handleGet.js';
-
+  import { handleGetAKA } from './handleGetAKA.js';
+  import { handleSaveAkaName } from './handleSaveAkaName.js';
+  import { handleDeleteAkaName } from './handleDeleteAkaName.js';
+  
   let HumanId = '';
   let FirstName = '';
   let MiddleName = '';
@@ -16,9 +19,17 @@
   let StartYear = '';
   let EndYear = '';
   let Notes = '';
+
+  let AkaNames = [];
+  let AKAHumanId= '';
+  let AKAFirstName = '';
+  let AKAMiddleName = '';
+  let AKALastName = '';
+
   let isLoading = true;
 
   let FormValid = false;
+  let AKAFormValid = false;
   async function setName(_FirstName, _MiddleName, _LastName, _StartYear, _EndYear, _Notes) {
     FirstName=_FirstName;
     MiddleName=_MiddleName;
@@ -29,13 +40,21 @@
   }
   $: {
     FormValid = FirstName && LastName && StartYear && EndYear && StartYear <= EndYear;
+    AKAFormValid = AKAFirstName && AKALastName;
+  }
+
+  async function setAkaNames(data) {
+    AkaNames = data;
   }
 
   onMount(async () => {
     const urlParams = new URLSearchParams(window.location.search);
     HumanId = urlParams.get("HumanId") || "";
     if (HumanId) {
-      handleGet(HumanId, setName);
+      await Promise.all([
+        handleGet(HumanId, setName),
+        handleGetAKA(HumanId, setAkaNames)
+      ]);
     }
     console.log("HumanId", HumanId);
     isLoading = false;
@@ -86,6 +105,29 @@
       <label class="label" for="Notes">Notes:</label>
       <div class="control">
         <textarea class="textarea" id="Notes" bind:value={Notes}></textarea>
+      </div>
+    </div>
+    <div class="ActionBox">
+      <label class="label" for="AkaNames">Also Known As:</label>
+      {#if AkaNames.length}
+        <ul>
+          {#each AkaNames as akaName}
+            <li>
+              {akaName.AKAFirstName} {akaName.AKAMiddleName} {akaName.AKALastName}
+              <button style="padding:0px;padding-left:5px;padding-right:5px;" on:click={() => handleDeleteAkaName(akaName.AKAHumanId, HumanId)}>X</button>
+            </li>
+          {/each}
+        </ul>
+      {:else}
+        <p>No also known as names added yet.</p>
+      {/if}
+      <div class="control">
+       
+        <input type="hidden" id="AKAHumanId" bind:value={AKAHumanId}>
+        <input class="input" type="text" id="AKAFirstName" placeholder="AKA First Name" bind:value={AKAFirstName}><br>
+        <input class="input" type="text" id="AKAMiddleName" placeholder="AKA Middle Name" bind:value={AKAMiddleName}><br>
+        <input class="input" type="text" id="AKALastName" placeholder="AKA Last Name" bind:value={AKALastName}><br>
+        <button class="button is-primary" type="button" on:click={() => handleSaveAkaName(AKAHumanId, HumanId, AKAFirstName, AKAMiddleName, AKALastName, AKAFormValid)}>Add Also Known As</button>
       </div>
     </div>
     <div class="field">
