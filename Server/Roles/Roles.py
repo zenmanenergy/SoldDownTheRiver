90eff852b11547d4e2d2fa1b1aeb73ef54cbcfd0
Steve Nelson <snelson@zenmanenergy.org>
2023-04-20 23:57:40 -0400

    initial commit

---
+++ Server/Roles/Roles.py
@@ -0,0 +1,17 @@
+from Lib import Database
+from flask import Blueprint, request
+from flask_cors import CORS, cross_origin
+from .GetRoles import get_roles
+
+blueprint = Blueprint('Roles', __name__)
+
+
+@blueprint.route("/GetRoles", methods=['GET'])
+@cross_origin()
+def GetRoles():
+    # Get the role data from the request
+    role_data = request.args.to_dict()
+
+    # Call the get_roles function from GetRoles.py
+    result = get_roles()
+    return result
