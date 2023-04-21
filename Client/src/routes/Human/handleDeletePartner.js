30f41daf309814a92d9b85ac4a9917545f3dc2c8
Steve Nelson <snelson@zenmanenergy.org>
2023-04-20 23:14:07 -0400

    Fixed Delete partner bug

--- Client/src/routes/Human/handleDeletePartner.js
+++ Client/src/routes/Human/handleDeletePartner.js
@@ -2,7 +2,8 @@ const baseURL = 'http://192.168.1.182';
 
 
 export async function handleDeletePartner(PartnerHumanId, HumanId) {
-    const response = await fetch(`${baseURL}/DeletePartner?PartnerHumanId=${PartnerHumanId}`);
+    console.log(`${baseURL}/DeletePartner?HumanId=${HumanId}&PartnerHumanId=${PartnerHumanId}`)
+    const response = await fetch(`${baseURL}/DeletePartner?HumanId=${HumanId}&PartnerHumanId=${PartnerHumanId}`);
     if (!response.ok) {
       const err = await response.json();
       console.error(err.message);
