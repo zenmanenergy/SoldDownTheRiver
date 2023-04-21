b2b10eef0810180c54f236cdec1ba86d8d34afc3
Steve Nelson <snelson@zenmanenergy.org>
2023-04-20 23:57:07 -0400

    function name change

--- Client/src/routes/Role/handleGet.js
+++ Client/src/routes/Role/handleGet.js
@@ -1,9 +1,9 @@
 // src/routes/Roles/handleGet.js
 const baseURL = 'http://192.168.1.182';
 
-export async function handleGet(RoleId, setName) {
+export async function handleGet(RoleId, setRole) {
     const response = await fetch(`${baseURL}/GetRole?RoleId=${RoleId}`);
     const data = await response.json();
   
-    setName(data.RoleId,data.Role);
+    setRole(data.RoleId,data.Role);
 }
