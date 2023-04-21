3564de7f8b8e8bb5a8c562a95543a5372b395e53
Steve Nelson <snelson@zenmanenergy.org>
2023-04-20 23:16:32 -0400

    insert partner for both humans

--- Server/Human/SavePartner.py
+++ Server/Human/SavePartner.py
@@ -6,8 +6,16 @@ def save_partner(HumanId, PartnerHumanId):
     cursor, connection = Database.ConnectToDatabase()
 
     # Check if the HumanId is present
-    query = "INSERT INTO Humans (HumanId, PartnerHumanId) VALUES (%s, %s)"
+    query = "INSERT INTO Partners (HumanId, PartnerHumanId) VALUES (%s, %s) ON DUPLICATE KEY UPDATE PartnerHumanId=values(PartnerHumanId),HumanId=values(HumanId)"
     values = (HumanId, PartnerHumanId)
+
+    # Execute the query and commit the changes
+    cursor.execute(query, values)
+    connection.commit()
+
+    # Check if the HumanId is present
+    query = "INSERT INTO Partners (HumanId, PartnerHumanId) VALUES (%s, %s) ON DUPLICATE KEY UPDATE PartnerHumanId=values(PartnerHumanId),HumanId=values(HumanId)"
+    values = (PartnerHumanId,HumanId)
 
     # Execute the query and commit the changes
     cursor.execute(query, values)
