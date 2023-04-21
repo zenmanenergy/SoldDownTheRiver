9815e24c477153843291d75a50284306ae280f11
Steve Nelson <snelson@zenmanenergy.org>
2023-04-20 23:14:18 -0400

    fixed GetPartners bug

--- Client/src/routes/Human/handleGetPartners.js
+++ Client/src/routes/Human/handleGetPartners.js
@@ -3,7 +3,7 @@ const baseURL = 'http://192.168.1.182';
 
 export async function handleGetPartners(HumanId, setPartners) {
   console.log(`${baseURL}/getPartners?HumanId=${HumanId}`)
-  const response = await fetch(`${baseURL}/getPartners?HumanId=${HumanId}`);
+  const response = await fetch(`${baseURL}/GetPartners?HumanId=${HumanId}`);
   const data = await response.json();
 
   setPartners(data);
