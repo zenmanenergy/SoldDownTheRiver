8c892293c71f00a2a0066d0c48730b2acf1684d7
Steve Nelson <snelson@zenmanenergy.org>
2023-04-20 23:57:23 -0400

    initial commit

---
+++ Server/Role/DeleteRole.py
@@ -0,0 +1,11 @@
+from Lib import Database
+
+def delete_role(RoleId):
+    # Delete the specified row from the Roles table
+    query = f"DELETE FROM Roles WHERE RoleId = '{RoleId}'"
+    cursor, connection = Database.ConnectToDatabase()
+    cursor.execute(query)
+    connection.commit()
+    connection.close()
+
+    return {'success': True,'message': f'Role "{RoleId}" deleted successfully.'}
