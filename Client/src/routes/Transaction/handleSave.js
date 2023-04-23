7241249ea068adcc1a690f8d64e7548051d04183
Steve Nelson <snelson@zenmanenergy.org>
2023-04-23 00:20:51 -0400

    initial commit

---
+++ Client/src/routes/Transaction/handleSave.js
@@ -0,0 +1,48 @@
+const baseURL = 'http://192.168.1.182';
+
+// src/routes/Transactions/handleSubmit.js
+export function handleSave(transactionId, transactionDate, fromHumanId, toHumanId, transactionType, notes, act, page, notaryHumanId, volume, url, userId, formValid) {
+
+  if (!formValid) {
+    const invalidFields = document.querySelectorAll("input:invalid");
+    if (invalidFields.length > 0) {
+      invalidFields[0].focus();
+    }
+    return;
+  }
+
+  const transactionData = {
+    TransactionId: transactionId,
+    TransactionDate: transactionDate,
+    FromHumanId: fromHumanId,
+    ToHumanId: toHumanId,
+    TransactionType: transactionType,
+    Notes: notes,
+    Act: act,
+    Page: page,
+    NotaryHumanId: notaryHumanId,
+    Volume: volume,
+    URL: url,
+    UserId: userId
+  };
+
+  const queryString = Object.keys(transactionData)
+    .map(key => key + '=' + encodeURIComponent(transactionData[key]))
+    .join('&');
+
+  const saveUrl = baseURL + '/Transaction/SaveTransaction?' + queryString; 
+  console.log(saveUrl)
+  fetch(saveUrl, {
+    method: 'GET'
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log("Save success", data);
+    window.location.href = '/Transactions';
+    // Handle the response data as needed
+  })
+  .catch(error => {
+    console.error("save error", error);
+    // Handle the error as needed
+  });
+}
