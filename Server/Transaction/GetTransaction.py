d8cb3e9e8b1de65ec159ee73cc673d83be66b9e7
Steve Nelson <snelson@zenmanenergy.org>
2023-04-23 00:21:40 -0400

    initial commit

---
+++ Server/Transaction/GetTransaction.py
@@ -0,0 +1,24 @@
+from Lib import Database
+
+def get_transaction(transaction_id):
+    if not transaction_id:
+        transaction_id = "-1"
+
+    # Connect to the database
+    cursor, connection = Database.ConnectToDatabase()
+
+    # Construct the SQL query
+    query = "SELECT * FROM Transactions WHERE TransactionId = %s"
+    values = (transaction_id,)
+
+    # Execute the query and get the results
+    cursor.execute(query, values)
+    result = cursor.fetchone()
+    if not result:
+        result = {}
+
+    # Close the database connection
+    connection.close()
+
+    # Return the result as a dictionary
+    return result
