ad23ab62f0aacfa6db1f769710d45963748f38bd
Steve Nelson <snelson@zenmanenergy.org>
2023-04-23 17:38:21 -0400

    initial commit

---
+++ Client/src/routes/Session.js
@@ -0,0 +1,59 @@
+const baseURL = 'http://192.168.1.182';
+class session {
+    SessionId = "";
+
+    async handleSession() {
+        // Add your session handling logic here
+        console.log("Handling session...");
+        this.GetSessionId();
+
+        await this.VerifySession(function(results) {
+            console.log("session verified!", results);
+        });
+    }
+
+    async VerifySession(callback) {
+        const VerifySessionData = {
+            SessionId: this.SessionId
+        };
+
+        const queryString = Object.keys(VerifySessionData)
+            .map(key => key + '=' + encodeURIComponent(VerifySessionData[key]))
+            .join('&');
+
+        const url = baseURL + '/Login/Verify?' + queryString;
+        console.log(url);
+        try {
+            const response = await fetch(url, {
+                method: 'GET'
+            });
+            const _SessionId = await response.json();
+            if (this.SessionId == _SessionId) {
+                Cookies.set("SessionId", this.SessionId, { expires: 365 });
+            } else {
+                Cookies.remove("SessionId");
+                window.location.href = '/Login?s=1';
+            }
+            callback(_SessionId);
+        } catch (error) {
+            console.error("save error", error);
+            // Handle the error as needed
+        }
+    }
+
+    GetSessionId() {
+        const urlParams = new URLSearchParams(window.location.search);
+
+        if (urlParams.get("SessionId")) {
+            this.SessionId = urlParams.get("SessionId");
+        } else if (Cookies.get("SessionId")) {
+            this.SessionId = Cookies.get("SessionId");
+        } else {
+            this.SessionId = "";
+            location.href = "/Login?s=1";
+        }
+        return true;
+    }
+}
+
+export const Session = new session();
