e0b9d4e6b3c907732e9aeb3e1f95a38ef0d59161
Steve Nelson <snelson@zenmanenergy.org>
2023-04-23 15:39:28 -0400

    added session variables

--- Server/Login/GetLogin.py
+++ Server/Login/GetLogin.py
@@ -1,4 +1,6 @@
 from Lib import Database
+import uuid
+import datetime
 
 def get_login(Email, Password):
     if not Email or not Password:
@@ -12,14 +14,27 @@ def get_login(Email, Password):
     values = (Email, Password,)
 
 
+    print(query % tuple(map(repr, values)))
+
+
     # Execute the query and get the results
     cursor.execute(query, values)
     result = cursor.fetchone()
     if not result:
         result={}
+    else:
+        query = "delete from userSessions where userId= %s "
+        values = (result['UserId'])
+        cursor.execute(query, values)
+
+        SessionId = str(uuid.uuid4())
+        query = "INSERT INTO userSessions(sessionId,userId,dateAdded) VALUES (%s, %s, %s)"
+        values = (SessionId, result['UserId'], datetime.datetime.now())
+        cursor.execute(query, values)
+        result=SessionId
         
     # Close the database connection
     connection.close()
     
     # Return the result as a dictionary
-    return result
+    return '"' + result+'"'
