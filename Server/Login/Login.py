4fff8c5a423f42731b683e995c7d2b283ed0922e
Steve Nelson <snelson@zenmanenergy.org>
2023-04-23 17:39:58 -0400

    Added verify login route

--- Server/Login/Login.py
+++ Server/Login/Login.py
@@ -3,6 +3,7 @@ from Lib import Database
 from flask import Blueprint, request
 from flask_cors import CORS, cross_origin
 from .GetLogin import get_login
+from .VerifySession import verify_session
 
 blueprint = Blueprint('Login', __name__)
 
@@ -21,3 +22,15 @@ def Login():
     return result
     
     
+@blueprint.route("/Login/Verify", methods=['GET'])
+@cross_origin()
+def Verify():
+    VerifyData = request.args.to_dict()
+
+    # Extract the Email and Password from the LoginData
+    SessionId = VerifyData.get('SessionId', None)
+
+    # Call the get_login function from GetLogin.py with the extracted data
+    result = verify_session(SessionId)
+
+    return result
