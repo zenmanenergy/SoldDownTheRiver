18c06f9253bc3427a0e897ec9964215f90470677
Steve Nelson <snelson@zenmanenergy.org>
2023-04-24 11:32:44 -0400

    initial commit

---
+++ Client/src/routes/Transaction/handleGetNotaryHumans.js
@@ -0,0 +1,10 @@
+// src/routes/Transactions/handleGet.js
+const baseURL = 'http://192.168.1.182';
+export async function handleGetNotaryHumans(SessionId,TransactionId, setTransactionData) {
+  const url="${baseURL}/Transaction/GetNotaryHumans?TransactionId=${TransactionId}&SessionId=${SessionId}"
+  console.log(url)
+  const response = await fetch(url);
+  const data = await response.json();
+
+  setTransactionData(data);
+}
