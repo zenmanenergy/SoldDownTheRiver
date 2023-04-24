efac1789cec4ca99d7d7501da221ef159881fdec
Steve Nelson <snelson@zenmanenergy.org>
2023-04-24 11:32:48 -0400

    initial commit

---
+++ Client/src/routes/Transaction/handleGetToHumans.js
@@ -0,0 +1,10 @@
+// src/routes/Transactions/handleGet.js
+const baseURL = 'http://192.168.1.182';
+export async function handleGetToHumans(SessionId,TransactionId, setTransactionData) {
+  const url="${baseURL}/Transaction/GetToHumans?TransactionId=${TransactionId}&SessionId=${SessionId}"
+  console.log(url)
+  const response = await fetch(url);
+  const data = await response.json();
+
+  setTransactionData(data);
+}
