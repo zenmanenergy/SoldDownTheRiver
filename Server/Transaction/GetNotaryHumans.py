82d3e4d8c62e43e46104ccd43183a4fe75c4f9e6
Steve Nelson <snelson@zenmanenergy.org>
2023-04-24 11:32:52 -0400

    initial commit

---
+++ Server/Transaction/GetNotaryHumans.py
@@ -0,0 +1,20 @@
+from Lib import Database
+
+def get_notary_humans():
+  
+
+    # Connect to the database
+    cursor, connection = Database.ConnectToDatabase()
+
+    # Construct the SQL query
+    query = "SELECT * FROM Humans order by Firstname, lastname"
+
+    # Execute the query and get the results
+    cursor.execute(query)
+    result = cursor.fetchall()
+
+    # Close the database connection
+    connection.close()
+
+    # Return the result as a dictionary
+    return result
