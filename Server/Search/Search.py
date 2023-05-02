2b795c87e0b83bf19c3e413a90c174997c0c0ea0
Monkeymilesmoo <miles@secretagents.com>
2023-05-02 18:40:18 -0400

    Can now search for all types

--- Server/Search/Search.py
+++ Server/Search/Search.py
@@ -5,6 +5,9 @@ from flask import Blueprint, request
 from flask_cors import CORS, cross_origin
 from .GetBusinesses import get_businesses
 from .GetHumans import get_humans
+from .GetLocations import get_locations
+from .GetRoles import get_roles
+from .GetTransactions import get_transactions
 
 blueprint = Blueprint('Search', __name__)
 
@@ -26,13 +29,58 @@ def SearchBusinesses():
 def SearchHumans():
     human_data = request.args.to_dict()
 
-    # Extract the BusinessId and BusinessName from the business_data
+
     FirstName = human_data.get('FirstName', None)
     LastName = human_data.get('LastName', None)
     MiddleName = human_data.get('MiddleName', None)
 
-    # Call the save_business function from SaveBusiness.py with the extracted data
+
     result = get_humans(FirstName, MiddleName, LastName)
+
+
+    return result
+
+@blueprint.route("/Search/Locations", methods=['GET'])
+@cross_origin()
+def SearchLocations():
+    location_data = request.args.to_dict()
+
+    
+    City = location_data.get('City', None)
+    State = location_data.get('State', None)
+    Country = location_data.get('Country', None)
+
+
+    result = get_locations(City, State, Country)
+
+
+    return result
+
+@blueprint.route("/Search/Roles", methods=['GET'])
+@cross_origin()
+def SearchRoles():
+    roles_data = request.args.to_dict()
+
+    
+    Role = roles_data.get('Role', None)
+
+
+    result = get_roles(Role)
+
+
+    return result
+
+@blueprint.route("/Search/Transactions", methods=['GET'])
+@cross_origin()
+def SearchTransactions():
+    transactions_data = request.args.to_dict()
+
+    
+    TransactionId = transactions_data.get('TransactionId', None)
+    TransactionDate = transactions_data.get('TransactionDate', None)
+
+
+    result = get_transactions(TransactionId, TransactionDate)
 
 
     return result
