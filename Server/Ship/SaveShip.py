0d9b98f4640085f60e640b6bffe1449efc5cf199
Steve Nelson <snelson@zenmanenergy.org>
2023-05-13 12:25:13 -0400

    initial commit

---
+++ Server/Ship/SaveShip.py
@@ -0,0 +1,30 @@
+import uuid
+from Lib import Database
+
+def save_ship(ShipId, ShipName,BuildDate, Notes, ShipType, Size):
+    # Connect to the database
+    cursor, connection = Database.ConnectToDatabase()
+
+    if len(BuildDate) == 0:
+        BuildDate = None
+    # Check if the ShipId is present
+    if ShipId:
+        # If the ShipId is present, update the existing ship
+        query = "UPDATE Ships SET ShipName=%s, BuildDate = %s, Notes = %s, ShipType = %s, Size = %s WHERE ShipId = %s"
+        values = (ShipName, BuildDate, Notes, ShipType, Size, ShipId)
+    else:
+        # If the ShipId is not present, create a new ship
+        ShipId = "SHP" + str(uuid.uuid4())
+        query = "INSERT INTO Ships (ShipId, ShipName,BuildDate, Notes, ShipType, Size) VALUES (%s, %s,  %s, %s, %s, %s)"
+        values = (ShipId, ShipName, BuildDate, Notes, ShipType, Size)
+
+    # Execute the query and commit the changes
+    print(query, values)
+    cursor.execute(query, values)
+    connection.commit()
+
+    # Close the database connection
+    connection.close()
+
+    # Return the ShipId as a JSON response
+    return {'success': True, 'ShipId': ShipId}
