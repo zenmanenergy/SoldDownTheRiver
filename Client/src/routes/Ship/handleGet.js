74d710a3dcffef369bc4e4cca4d39a11a406bdb7
Steve Nelson <snelson@zenmanenergy.org>
2023-05-13 12:24:45 -0400

    initial commit

---
+++ Client/src/routes/Ship/handleGet.js
@@ -0,0 +1,11 @@
+// src/routes/Ships/handleGet.js
+import { baseURL } from '../Settings';
+
+export async function handleGet(SessionId, ShipId, callback) {
+  const url = `${baseURL}/Ship/GetShip?ShipId=${ShipId}&SessionId=${SessionId}`;
+  console.log(url);
+  const response = await fetch(url);
+  const data = await response.json();
+
+  callback(data);
+}
