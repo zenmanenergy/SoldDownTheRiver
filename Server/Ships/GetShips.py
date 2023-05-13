6bb6c8f2edc2fdf867c313854491b89197316e25
Steve Nelson <snelson@zenmanenergy.org>
2023-05-13 12:25:20 -0400

    initial commit

---
+++ Server/Ships/GetShips.py
@@ -0,0 +1,22 @@
+from Lib import Database
+
+def get_ships():
+    # Connect to the database
+    cursor, connection = Database.ConnectToDatabase()
+
+    # Construct the SQL query
+    query = "SELECT *, (select max(dateAdded) from History where History.KeyValue=Ships.ShipId  and History.TableName='Ships' and History.KeyName='ShipId') LastModified"
+    query += "  FROM Ships ORDER BY ShipType, Size"
+    values = ()
+
+    # Execute the query and get the results
+    cursor.execute(query, values)
+    result = cursor.fetchall()
+    if not result:
+        result = []
+
+    # Close the database connection
+    connection.close()
+
+    # Return the result
+    return result
