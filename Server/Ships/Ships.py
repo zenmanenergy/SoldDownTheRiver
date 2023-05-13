e773995258d1341845d5eca4b464157e63f1d516
Steve Nelson <snelson@zenmanenergy.org>
2023-05-13 12:25:23 -0400

    initial commit

---
+++ Server/Ships/Ships.py
@@ -0,0 +1,16 @@
+from Lib import Database
+from flask import Blueprint, request
+from flask_cors import CORS, cross_origin
+from .GetShips import get_ships
+
+blueprint = Blueprint('Ships', __name__)
+
+@blueprint.route("/Ships/GetShips", methods=['GET'])
+@cross_origin()
+def GetShips():
+    # Get the user data from the request
+    user_data = request.args.to_dict()
+
+    # Call the get_ships function from GetShips.py
+    result = get_ships()
+    return result
