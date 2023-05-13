c47486dd821b963987abf1d866492af3c35b8f39
Steve Nelson <snelson@zenmanenergy.org>
2023-05-13 11:11:15 -0400

    initial commit

---
+++ Client/src/routes/Business/handleGetLocations.js
@@ -0,0 +1,10 @@
+import { baseURL } from '../Settings';
+
+export async function handleGetLocations(SessionId, callback) {
+  const url=`${baseURL}/Business/GetLocations?SessionId=${SessionId}`
+  console.log(url)
+  const response = await fetch(url);
+  const locations = await response.json();
+  callback(locations);
+}
+
