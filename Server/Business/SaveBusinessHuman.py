3e10f23999a1429605e3dbd59fdbe285770a7d77
Steve Nelson <snelson@zenmanenergy.org>
2023-05-13 10:52:07 -0400

    added roleId to the function

--- Server/Business/SaveBusinessHuman.py
+++ Server/Business/SaveBusinessHuman.py
@@ -1,13 +1,13 @@
 import uuid
 from Lib import Database
 
-def save_BusinessHuman(BusinessId, HumanId):
+def save_BusinessHuman(BusinessId, HumanId, RoleId):
     # Connect to the database
     cursor, connection = Database.ConnectToDatabase()
 
 
-    query = "INSERT INTO BusinessHumans (BusinessId, HumanId) VALUES (%s, %s)"
-    values = (BusinessId, HumanId)
+    query = "INSERT INTO BusinessHumans (BusinessId, HumanId, RoleId) VALUES (%s, %s, %s) ON DUPLICATE KEY UPDATE RoleId = VALUES(RoleId)"
+    values = (BusinessId, HumanId, RoleId)
 
     # Execute the query and commit the changes
     cursor.execute(query, values)
