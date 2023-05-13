1f4227fa0f29c45b94a6a70fed8aa0ae572d776f
Steve Nelson <snelson@zenmanenergy.org>
2023-05-13 10:37:17 -0400

    initial commit

---
+++ Server/Transaction/DeleteTransactionHuman.py
@@ -0,0 +1,11 @@
+from Lib import Database
+
+def delete_transactionhuman(TransactionId, HumanId):
+    # Delete the specified row from the Transactions table
+    query = f"DELETE FROM TransactionHumans WHERE TransactionId = '{TransactionId}' and  HumanId = '{HumanId}'"
+    cursor, connection = Database.ConnectToDatabase()
+    cursor.execute(query)
+    connection.commit()
+    connection.close()
+
+    return {'success': True,'message': f'Transaction with TransactionId {TransactionId} HumanId {HumanId} deleted successfully.'}
