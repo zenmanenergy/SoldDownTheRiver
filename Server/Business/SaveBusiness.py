12eb0e32dd081d04f70aa4e4301e5b463ac2e26a
Steve Nelson <snelson@zenmanenergy.org>
2023-05-13 11:12:10 -0400

    added LocationId to save_business

--- Server/Business/SaveBusiness.py
+++ Server/Business/SaveBusiness.py
@@ -1,20 +1,20 @@
 import uuid
 from Lib import Database
 
-def save_business(BusinessId, BusinessName):
+def save_business(BusinessId, BusinessName, LocationId):
     # Connect to the database
     cursor, connection = Database.ConnectToDatabase()
 
     # Check if the BusinessId is present
     if BusinessId:
         # If the BusinessId is present, update the existing business
-        query = "UPDATE Businesses SET BusinessName = %s WHERE BusinessId = %s"
-        values = (BusinessName, BusinessId)
+        query = "UPDATE Businesses SET BusinessName = %s, LocationId=%s WHERE BusinessId = %s "
+        values = (BusinessName, LocationId, BusinessId)
     else:
         # If the BusinessId is not present, create a new business
         BusinessId = "BUS"+str(uuid.uuid4())
-        query = "INSERT INTO Businesses (BusinessId, BusinessName) VALUES (%s, %s)"
-        values = (BusinessId, BusinessName)
+        query = "INSERT INTO Businesses (BusinessId, LocationId, BusinessName) VALUES (%s, %s, %s)"
+        values = (BusinessId, LocationId, BusinessName)
 
     # Execute the query and commit the changes
     cursor.execute(query, values)
