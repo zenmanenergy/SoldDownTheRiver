c1284280431ad7c0084a1c3f2c6eb0444097670c
Steve Nelson <snelson@zenmanenergy.org>
2023-05-13 11:11:08 -0400

    change the callback function

--- Client/src/routes/Business/handleGet.js
+++ Client/src/routes/Business/handleGet.js
@@ -1,12 +1,12 @@
 // src/routes/Businesses/handleGet.js
 import { baseURL } from '../Settings';
 // src/routes/Business/handleGet.js
-export async function handleGet(SessionId, BusinessId, setName) {
+export async function handleGet(SessionId, BusinessId, callback) {
     const url = `${baseURL}/Business/GetBusiness?BusinessId=${BusinessId}&SessionId=${SessionId}`
     console.log(url)
     const response = await fetch(url);
     const data = await response.json();
   
-    setName(data.BusinessName, data.LastModified);
+    callback(data);
 }
   
