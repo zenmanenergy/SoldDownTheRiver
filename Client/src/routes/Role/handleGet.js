dadfe80c0207410658bff95028360d7b4503135e
Steve Nelson <snelson@zenmanenergy.org>
2023-05-16 02:31:32 -0400

    changed the export added a callback

--- Client/src/routes/Role/handleGet.js
+++ Client/src/routes/Role/handleGet.js
@@ -1,11 +1,11 @@
 // src/routes/Roles/handleGet.js
 import { baseURL } from '../Settings';
 
-export async function handleGet(SessionId,RoleId, setRole) {
+export async function handleGet(SessionId,RoleId,callback) {
     const url=`${baseURL}/Role/GetRole?RoleId=${RoleId}&SessionId=${SessionId}`
     console.log(url)
     const response = await fetch(url);
     const data = await response.json();
   
-    setRole(data.RoleId,data.Role, data.LastModified);
+    callback(data);
 }
