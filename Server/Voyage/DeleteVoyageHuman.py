f5be46c8d359d6c7ca22fd5a2975577cfb5b42f6
Steve Nelson <snelson@zenmanenergy.org>
2023-05-16 16:42:52 -0400

    initial commit

---
+++ Server/Voyage/DeleteVoyageHuman.py
@@ -0,0 +1,11 @@
+from Lib import Database
+
+def delete_VoyageHuman(VoyageId, HumanId):
+    # Delete the specified row from the Voyages table
+    query = f"DELETE FROM VoyageHumans WHERE VoyageId = '{VoyageId}' and  HumanId = '{HumanId}'"
+    cursor, connection = Database.ConnectToDatabase()
+    cursor.execute(query)
+    connection.commit()
+    connection.close()
+
+    return {'success': True,'message': f'Voyage with VoyageId {VoyageId} HumanId {HumanId} deleted successfully.'}
