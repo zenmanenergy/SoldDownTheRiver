b2260b732f5b06991633497c5aa2053baacc48db
Steve Nelson <snelson@zenmanenergy.org>
2023-05-16 02:31:14 -0400

    changed the export added a callback

--- Client/src/routes/Location/handleGet.js
+++ Client/src/routes/Location/handleGet.js
@@ -1,11 +1,11 @@
 // src/routes/Locations/handleGet.js
 import { baseURL } from '../Settings';
 // src/routes/Locations/handleGet.js
-export async function handleGet(SessionId,LocationId, setLocation) {
+export async function handleGet(SessionId,LocationId, callback) {
     const url=`${baseURL}/Location/GetLocation?LocationId=${LocationId}&SessionId=${SessionId}`
     console.log(url)
     const response = await fetch(url);
     const data = await response.json();
   
-    setLocation(data.City, data.State, data.Country, data.Latitude, data.Longitude, data.LastModified);
+    callback(data);
 }
