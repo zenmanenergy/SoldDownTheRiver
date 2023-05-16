69c39adcb4d31506a1cdade7cc4933c7ab3c8115
Steve Nelson <snelson@zenmanenergy.org>
2023-05-16 02:29:53 -0400

    added a space

--- Client/src/routes/Business/handleGet.js
+++ Client/src/routes/Business/handleGet.js
@@ -1,6 +1,5 @@
-// src/routes/Businesses/handleGet.js
 import { baseURL } from '../Settings';
-// src/routes/Business/handleGet.js
+
 export async function handleGet(SessionId, BusinessId, callback) {
     const url = `${baseURL}/Business/GetBusiness?BusinessId=${BusinessId}&SessionId=${SessionId}`
     console.log(url)
