c536153f9c2d953bfdc8a0e2ec8cbb120af5cea2
Steve Nelson <snelson@zenmanenergy.org>
2023-05-16 02:31:05 -0400

    changed the export added a callback

--- Client/src/routes/Businesses/handleGet.js
+++ Client/src/routes/Businesses/handleGet.js
@@ -1,14 +1,9 @@
 import { baseURL } from '../Settings';
 
-async function handleGet(SessionId) {
+export async function handleGet(SessionId, callback) {
   const url=`${baseURL}/Businesses/GetBusinesses?SessionId=${SessionId}`
   console.log(url)
   const response = await fetch(url);
   const businesses = await response.json();
-  return businesses;
-
-  
-
+  callback(businesses);
 }
-
-export default handleGet;
