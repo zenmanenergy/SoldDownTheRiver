4c77062d751405147eb26423a58261650db473d6
Steve Nelson <snelson@zenmanenergy.org>
2023-05-17 22:42:09 -0400

    added roleId and notes

--- Server/Voyage/SaveVoyageHuman.py
+++ Server/Voyage/SaveVoyageHuman.py
@@ -2,7 +2,7 @@ import uuid
 from Lib import Database
 import datetime
 
-def save_VoyageHuman(VoyageId, HumanId):
+def save_VoyageHuman(VoyageId, HumanId, RoleId, Notes):
     
     # Connect to the database
     cursor, connection = Database.ConnectToDatabase()
@@ -10,10 +10,10 @@ def save_VoyageHuman(VoyageId, HumanId):
 
 
     
-    query = "INSERT INTO VoyageHumans (VoyageId, HumanId) VALUES (%s, %s)"
-    query +=" ON DUPLICATE KEY UPDATE VoyageId=values(VoyageId),HumanId=values(HumanId)"
+    query = "INSERT INTO VoyageHumans (VoyageId, HumanId, RoleId, Notes) VALUES (%s, %s,%s, %s)"
+    query +=" ON DUPLICATE KEY UPDATE VoyageId=values(VoyageId),HumanId=values(HumanId) ,RoleId=values(RoleId), Notes=values(Notes)"
         
-    values = (VoyageId, HumanId,)
+    values = (VoyageId, HumanId,RoleId, Notes,)
 
     print(query % values)
 
