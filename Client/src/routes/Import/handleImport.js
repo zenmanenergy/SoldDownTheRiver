749284c63f4b5350fc8eaf2ee72a07bf4ce9c306
Steve Nelson <snelson@zenmanenergy.org>
2023-05-24 01:19:37 -0400

    initial commit

---
+++ Client/src/routes/Import/handleImport.js
@@ -0,0 +1,29 @@
+import { baseURL } from '../Settings';
+
+export function handleImport(SessionId,SpreadsheetName, SpreadsheetData) {
+  
+  let postData={}
+  postData.SessionId= SessionId
+  postData.SpreadsheetName= SpreadsheetName
+  postData.SpreadsheetData= SpreadsheetData
+
+  postData = JSON.stringify(postData)
+  const url = baseURL + '/Import/ImportData'; 
+  console.log(url)
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: postData
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log("Save success",data);
+    // Handle the response data as needed
+  })
+  .catch(error => {
+    console.error("save error",error);
+    // Handle the error as needed
+  });
+}
