44dede9d4ba724333bee187f25a1748d4dbbe535
Steve Nelson <snelson@zenmanenergy.org>
2023-06-03 13:49:09 -0400

    fixed tablename

--- Server/Business/GetBusiness.py
+++ Server/Business/GetBusiness.py
@@ -8,7 +8,7 @@ def get_business(BusinessId):
     cursor, connection = Database.ConnectToDatabase()
 
     # Construct the SQL query
-    query = "SELECT *, (select max(dateAdded) from History where History.KeyValue=Businesses.BusinessId and History.TableName='Business' and History.KeyName='BusinessId') LastModified"
+    query = "SELECT *, (select max(dateAdded) from History where History.KeyValue=Businesses.BusinessId and History.TableName='Businesses' and History.KeyName='BusinessId') LastModified"
     query += " FROM Businesses WHERE BusinessId = %s"
     values = (BusinessId,)
 
