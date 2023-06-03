7a721c286208e9ea59d4a916e658a1f388b95322
Steve Nelson <snelson@zenmanenergy.org>
2023-06-03 13:48:43 -0400

    fixed capital letters

---
+++ Database/fix_table_cases.sql
@@ -0,0 +1,84 @@
+ALTER TABLE `SoldDownTheRiver`.`families_` 
+RENAME TO  `SoldDownTheRiver`.`Families` ;
+
+ALTER TABLE `SoldDownTheRiver`.`history` 
+RENAME TO  `SoldDownTheRiver`.`History_` ;
+ALTER TABLE `SoldDownTheRiver`.`humanowners` 
+RENAME TO  `SoldDownTheRiver`.`HumanOwners_` ;
+
+ALTER TABLE `SoldDownTheRiver`.`humans` 
+RENAME TO  `SoldDownTheRiver`.`Humans_` ;
+
+ALTER TABLE `SoldDownTheRiver`.`locations` 
+RENAME TO  `SoldDownTheRiver`.`Locations_` ;
+
+ALTER TABLE `SoldDownTheRiver`.`roles` 
+RENAME TO  `SoldDownTheRiver`.`Roles_` ;
+
+ALTER TABLE `SoldDownTheRiver`.`ships` 
+RENAME TO  `SoldDownTheRiver`.`Ships_` ;
+
+ALTER TABLE `SoldDownTheRiver`.`transactionhumans` 
+RENAME TO  `SoldDownTheRiver`.`TransactionHumans_` ;
+
+ALTER TABLE `SoldDownTheRiver`.`transactions` 
+RENAME TO  `SoldDownTheRiver`.`Transactions_` ;
+
+ALTER TABLE `SoldDownTheRiver`.`users` 
+RENAME TO  `SoldDownTheRiver`.`Users_` ;
+
+ALTER TABLE `SoldDownTheRiver`.`usersessions` 
+RENAME TO  `SoldDownTheRiver`.`UserSessions_` ;
+
+ALTER TABLE `SoldDownTheRiver`.`voyagecargo` 
+RENAME TO  `SoldDownTheRiver`.`VoyageCargo_` ;
+
+ALTER TABLE `SoldDownTheRiver`.`voyagehumans` 
+RENAME TO  `SoldDownTheRiver`.`VoyageHumans_` ;
+
+ALTER TABLE `SoldDownTheRiver`.`voyages` 
+RENAME TO  `SoldDownTheRiver`.`Voyages_` ;
+
+
+-- 
+
+ALTER TABLE `SoldDownTheRiver`.`Families_` 
+RENAME TO  `SoldDownTheRiver`.`Families` ;
+
+ALTER TABLE `SoldDownTheRiver`.`History_` 
+RENAME TO  `SoldDownTheRiver`.`History` ;
+ALTER TABLE `SoldDownTheRiver`.`HumanOwners_` 
+RENAME TO  `SoldDownTheRiver`.`HumanOwners` ;
+
+ALTER TABLE `SoldDownTheRiver`.`Humans_` 
+RENAME TO  `SoldDownTheRiver`.`Humans` ;
+
+ALTER TABLE `SoldDownTheRiver`.`Locations_` 
+RENAME TO  `SoldDownTheRiver`.`Locations` ;
+
+ALTER TABLE `SoldDownTheRiver`.`Roles_` 
+RENAME TO  `SoldDownTheRiver`.`Roles` ;
+
+ALTER TABLE `SoldDownTheRiver`.`Ships_` 
+RENAME TO  `SoldDownTheRiver`.`Ships` ;
+
+ALTER TABLE `SoldDownTheRiver`.`TransactionHumans_` 
+RENAME TO  `SoldDownTheRiver`.`TransactionHumans` ;
+
+ALTER TABLE `SoldDownTheRiver`.`Transactions_` 
+RENAME TO  `SoldDownTheRiver`.`Transactions` ;
+
+ALTER TABLE `SoldDownTheRiver`.`Users_` 
+RENAME TO  `SoldDownTheRiver`.`Users` ;
+
+ALTER TABLE `SoldDownTheRiver`.`UserSessions_` 
+RENAME TO  `SoldDownTheRiver`.`UserSessions` ;
+
+ALTER TABLE `SoldDownTheRiver`.`VoyageCargo_` 
+RENAME TO  `SoldDownTheRiver`.`VoyageCargo` ;
+
+ALTER TABLE `SoldDownTheRiver`.`VoyageHumans_` 
+RENAME TO  `SoldDownTheRiver`.`VoyageHumans` ;
+
+ALTER TABLE `SoldDownTheRiver`.`Voyages_` 
+RENAME TO  `SoldDownTheRiver`.`Voyages` ;
