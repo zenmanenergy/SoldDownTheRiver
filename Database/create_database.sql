6819e3afcc55a0d2c1973d5a4f9e3aacc8705bcf
Steve Nelson <snelson@zenmanenergy.org>
2023-06-03 13:48:34 -0400

    added a few columns. fixed capitals

--- Database/create_database.sql
+++ Database/create_database.sql
@@ -1,25 +1,25 @@
-CREATE TABLE `businesses` (
+CREATE TABLE `Businesses` (
   `BusinessId` char(39) NOT NULL,
-  `BusinessName` varchar(45) DEFAULT NULL,
+  `BusinessName` varchar(255) DEFAULT NULL,
   `LocationId` char(39) DEFAULT NULL,
   PRIMARY KEY (`BusinessId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
-CREATE TABLE `businesshumans` (
+CREATE TABLE `BusinessHumans` (
   `BusinessId` char(39) NOT NULL,
   `HumanId` char(39) NOT NULL,
   `RoleId` char(39) DEFAULT NULL,
   PRIMARY KEY (`BusinessId`,`HumanId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
-CREATE TABLE `families` (
+CREATE TABLE `Families` (
   `HumanId` char(39) NOT NULL,
   `FamilyHumanId` char(39) NOT NULL,
   `Relationship` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`HumanId`,`FamilyHumanId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
-CREATE TABLE `history` (
+CREATE TABLE `History` (
   `HistoryId` char(39) NOT NULL,
   `TableName` varchar(45) DEFAULT NULL,
   `KeyName` varchar(45) DEFAULT NULL,
@@ -30,66 +30,60 @@ CREATE TABLE `history` (
   PRIMARY KEY (`HistoryId`),
   KEY `tableKeyValue` (`TableName`,`KeyName`,`KeyValue`,`UserId`),
   KEY `tableKey` (`TableName`,`KeyName`,`UserId`) /*!80000 INVISIBLE */
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
-CREATE TABLE `humanroles` (
+CREATE TABLE `HumanOwners` (
   `HumanId` char(39) NOT NULL,
-  `RoleId` varchar(45) NOT NULL,
-  PRIMARY KEY (`HumanId`,`RoleId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `OwnerBusinessId` char(39) NOT NULL,
+  `OwnDate` datetime DEFAULT NULL,
+  `TransactionId` char(39) DEFAULT NULL,
+  PRIMARY KEY (`HumanId`,`OwnerBusinessId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
-CREATE TABLE `humans` (
+CREATE TABLE `Humans` (
   `HumanId` char(39) NOT NULL,
-  `FirstName` varchar(45) DEFAULT NULL,
+  `FirstName` varchar(155) DEFAULT NULL,
   `MiddleName` varchar(45) DEFAULT NULL,
-  `LastName` varchar(45) DEFAULT NULL,
-  `StartYear` varchar(10) DEFAULT NULL,
-  `EndYear` varchar(10) DEFAULT NULL,
+  `LastName` varchar(155) DEFAULT NULL,
   `Notes` text,
   `BirthDate` datetime DEFAULT NULL,
+  `BirthDateAccuracy` varchar(45) DEFAULT NULL,
   `BirthPlace` varchar(45) DEFAULT NULL,
   `RaceId` char(39) DEFAULT NULL,
   `PhysicalDescription` varchar(150) DEFAULT NULL,
-  `gender` varchar(45) DEFAULT NULL,
+  `Gender` varchar(45) DEFAULT NULL,
+  `HeightInInches` float DEFAULT NULL,
   PRIMARY KEY (`HumanId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
-CREATE TABLE `humansaka` (
+CREATE TABLE `HumansAKA` (
   `AKAHumanId` char(39) NOT NULL,
   `HumanId` char(39) NOT NULL,
   `AKAFirstName` varchar(45) DEFAULT NULL,
   `AKAMiddleName` varchar(45) DEFAULT NULL,
   `AKALastName` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`AKAHumanId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
-CREATE TABLE `locations` (
+CREATE TABLE `Locations` (
   `LocationId` char(39) NOT NULL,
-  `Address` varchar(45) DEFAULT NULL,
-  `City` varchar(45) DEFAULT NULL,
-  `County` varchar(45) DEFAULT NULL,
-  `State` varchar(45) DEFAULT NULL,
-  `Country` varchar(45) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `City` varchar(255) DEFAULT NULL,
+  `County` varchar(255) DEFAULT NULL,
+  `State` varchar(255) DEFAULT NULL,
+  `Country` varchar(255) DEFAULT NULL,
   `Latitude` float DEFAULT NULL,
   `Longitude` float DEFAULT NULL,
   PRIMARY KEY (`LocationId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
-CREATE TABLE `notary` (
-  `HumanId` char(39) NOT NULL,
-  `Language` varchar(45) DEFAULT NULL,
-  `StartYear` int DEFAULT NULL,
-  `EndYear` int DEFAULT NULL,
-  PRIMARY KEY (`HumanId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `roles` (
+CREATE TABLE `Roles` (
   `RoleId` char(39) NOT NULL,
   `Role` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`RoleId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
-CREATE TABLE `ships` (
+CREATE TABLE `Ships` (
   `ShipId` char(39) NOT NULL,
   `ShipName` varchar(45) DEFAULT NULL,
   `BuildDate` datetime DEFAULT NULL,
@@ -97,36 +91,39 @@ CREATE TABLE `ships` (
   `ShipType` varchar(45) DEFAULT NULL,
   `Size` varchar(45) DEFAULT NULL,
   `OwnerBusinessId` char(39) DEFAULT NULL,
+  `HomePortLocationId` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`ShipId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
-CREATE TABLE `transactionhumans` (
+CREATE TABLE `TransactionHumans` (
   `TransactionId` char(39) NOT NULL,
   `HumanId` char(39) NOT NULL,
-  `Notes` varchar(45) DEFAULT NULL,
+  `Notes` text,
   `Price` float DEFAULT NULL,
   PRIMARY KEY (`TransactionId`,`HumanId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
-CREATE TABLE `transactions` (
+CREATE TABLE `Transactions` (
   `TransactionId` char(39) NOT NULL,
   `TransactionDate` datetime DEFAULT NULL,
   `FromBusinessId` char(39) DEFAULT NULL,
   `ToBusinessId` char(39) DEFAULT NULL,
   `TransactionType` varchar(45) DEFAULT NULL,
-  `Notes` varchar(255) DEFAULT NULL,
-  `Act` varchar(10) DEFAULT NULL,
-  `Page` varchar(10) DEFAULT NULL,
+  `Notes` text,
+  `Act` varchar(155) DEFAULT NULL,
+  `Page` varchar(155) DEFAULT NULL,
   `NotaryBusinessId` char(39) DEFAULT NULL,
-  `Volume` varchar(10) DEFAULT NULL,
+  `Volume` varchar(155) DEFAULT NULL,
   `URL` varchar(255) DEFAULT NULL,
-  `TranscriberId` char(39) DEFAULT NULL,
-  `price` float DEFAULT NULL,
   `NeedsReview` int DEFAULT NULL,
+  `TranscriberUserId` char(39) DEFAULT NULL,
+  `TotalPrice` float DEFAULT NULL,
+  `FromLocationId` char(39) DEFAULT NULL,
+  `ToLocationId` char(39) DEFAULT NULL,
   PRIMARY KEY (`TransactionId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
-CREATE TABLE `users` (
+CREATE TABLE `Users` (
   `UserId` char(39) NOT NULL,
   `FirstName` varchar(45) DEFAULT NULL,
   `LastName` varchar(45) DEFAULT NULL,
@@ -137,16 +134,16 @@ CREATE TABLE `users` (
   `Phone` varchar(45) DEFAULT NULL,
   `UserType` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`UserId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
-CREATE TABLE `usersessions` (
+CREATE TABLE `UserSessions` (
   `SessionId` char(39) DEFAULT NULL,
   `UserId` char(39) NOT NULL,
   `DateAdded` datetime DEFAULT NULL,
   PRIMARY KEY (`UserId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
-CREATE TABLE `voyagecargo` (
+CREATE TABLE `VoyageCargo` (
   `CargoId` char(39) NOT NULL,
   `VoyageId` char(39) DEFAULT NULL,
   `Description` varchar(255) DEFAULT NULL,
@@ -154,26 +151,30 @@ CREATE TABLE `voyagecargo` (
   `Value` float DEFAULT NULL,
   `Notes` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`CargoId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
-CREATE TABLE `voyagehumans` (
+CREATE TABLE `VoyageHumans` (
   `VoyageId` char(39) NOT NULL,
-  `HumanId` char(39) DEFAULT NULL,
+  `HumanId` char(39) NOT NULL,
   `RoleId` char(39) DEFAULT NULL,
   `Notes` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`VoyageId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `OwnerStartBusinessId` char(39) DEFAULT NULL,
+  `OwnerEndBusinessId` char(39) DEFAULT NULL,
+  `ShippingAgentBusinessId` char(39) DEFAULT NULL,
+  PRIMARY KEY (`VoyageId`,`HumanId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
-CREATE TABLE `voyages` (
+CREATE TABLE `Voyages` (
   `VoyageId` char(39) NOT NULL,
   `ShipId` char(39) DEFAULT NULL,
+  `CaptainHumanId` char(39) DEFAULT NULL,
   `StartLocationId` char(39) DEFAULT NULL,
   `EndLocationId` char(39) DEFAULT NULL,
   `StartDate` datetime DEFAULT NULL,
   `EndDate` datetime DEFAULT NULL,
   `Notes` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`VoyageId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
 
 
