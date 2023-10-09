8cd78d056b04a22f04f11a2d11699c6905237a8f
Steve Nelson <snelson@zenmanenergy.org>
2023-10-09 11:52:26 -0400

    switching from businesses

--- Server/Ship/GetOwners.py
+++ Server/Ship/GetOwners.py
@@ -2,23 +2,24 @@ from Lib import Database
 
 def get_owners(ShipId):
 
-    # Connect to the database
-    cursor, connection = Database.ConnectToDatabase()
+	# Connect to the database
+	cursor, connection = Database.ConnectToDatabase()
 
-    # Construct the SQL query
-    query = "SELECT *, (select max(dateAdded) from History where History.KeyValue=Businesses.BusinessId and History.TableName='Businesses' and History.KeyName='BusinessId') LastModified"
-    query +=" FROM Businesses order by BusinessName"
-    values = ()
+	# Construct the SQL query
+	query = f"SELECT *"
+	# , (select max(dateAdded) from History where History.KeyValue=Businesses.BusinessId and History.TableName='Businesses' and History.KeyName='BusinessId') LastModified"
+	query +=f" FROM  humans join humanroles on humans.HumanId=humanroles.HumanId join roles on humanroles.RoleId=humanroles.RoleId"
+	query += f" where roles.Role='Ship Owner'"
+	print(query)
 
+	# Execute the query and get the results
+	cursor.execute(query)
+	result = cursor.fetchall()
+	if not result:
+		result=[]
+		
+	# Close the database connection
+	connection.close()
 
-    # Execute the query and get the results
-    cursor.execute(query, values)
-    result = cursor.fetchall()
-    if not result:
-        result=[]
-        
-    # Close the database connection
-    connection.close()
-
-    # Return the result as a dictionary
-    return result
+	# Return the result as a dictionary
+	return result
