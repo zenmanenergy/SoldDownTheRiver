94ce0b55444ca551da752a8bdb2e6d677fa1b9d4
Monkeymilesmoo <miles@secretagents.com>
2023-10-09 17:19:26 -0400

    why is this happening, fix it please pretty please this is bad idk what is going on

---
+++ Client/src/routes/Test/SelectHuman.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { onMount } from "svelte";
+
+	let humans = [];
+	
+	import { onMount } from "svelte";
+
+onMount(async () => {
+	const selectElement = document.querySelector("#human-select");
+	
+	// Fetch data using the Fetch API
+	const response = await fetch("http://localhost:80/Humans/GetHumans?Query=abc");
+	const data = await response.json();
+
+	// Process the results
+	const processedData = data.map(human => ({
+		id: human.HumanId,
+		text: `${human.FirstName || ''} ${human.MiddleName || ''} ${human.LastName || ''}`.trim()
+	}));
+
+	// Initialize select2 with fetched data
+	$(selectElement).select2({
+		data: processedData,
+		placeholder: 'Select a human',
+		minimumInputLength: 1
+	});
+});
+
+</script>
+
+<select id="human-select">
+</select>
