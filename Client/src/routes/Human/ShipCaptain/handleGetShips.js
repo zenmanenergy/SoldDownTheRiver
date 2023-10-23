24b341cef0f98fbcb5a12fcf661282819c35d7fb
Steve Nelson <snelson@zenmanenergy.org>
2023-10-23 19:55:00 -0400

    initial commit

---
+++ Client/src/routes/Human/ShipCaptain/handleGetShips.js
@@ -0,0 +1,17 @@
+import { baseURL } from '../../Settings';
+import { SuperFetch } from '../../SuperFetch';
+
+export async function handleGetShips(SessionId,HumanId, callback) {
+	
+
+	const Data = {
+		SessionId:SessionId,
+		HumanId:HumanId
+	};
+	const url = baseURL + '/Human/ShipOwner/GetShips?'; 
+	console.log(url)
+	const FormValid=true
+	let data = await SuperFetch(url, Data, FormValid)
+
+	callback(data);
+}
