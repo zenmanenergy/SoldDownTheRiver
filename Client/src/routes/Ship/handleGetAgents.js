aa2708aab81f0fb0a11200bd4a1c7a913bfc527a
Steve Nelson <snelson@zenmanenergy.org>
2023-10-23 19:58:19 -0400

    initial commit

---
+++ Client/src/routes/Ship/handleGetAgents.js
@@ -0,0 +1,15 @@
+import { baseURL } from '../Settings';
+import { SuperFetch } from '../SuperFetch';
+
+export async function handleGetAgents(SessionId, Query,callback) {
+
+	const Data = {
+		SessionId:SessionId,
+		Query:Query,
+	}
+	const url = baseURL + '/Ship/GetAgents?'
+	const FormValid=true
+	let data = await SuperFetch(url, Data, FormValid)
+
+	callback(data);
+}
