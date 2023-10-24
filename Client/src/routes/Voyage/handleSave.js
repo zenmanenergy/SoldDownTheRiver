937f7940b505ca02f0c018a2c2f96c0a95b0c0c1
Steve Nelson <snelson@zenmanenergy.org>
2023-10-23 20:04:21 -0400

    fixed a date bug

--- Client/src/routes/Voyage/handleSave.js
+++ Client/src/routes/Voyage/handleSave.js
@@ -3,24 +3,25 @@ import { SuperFetch } from '../SuperFetch';
 
 export async function handleSave(SessionId,Data,FormValid) {
 
-	if (Data.StartDate instanceof Date) {
-		Data.StartDate = Data.StartDate.toISOString();
-	} else {
-		Data.StartDate = "";
-	}
-	if (Data.EndDate instanceof Date) {
-		Data.EndDate = Data.EndDate.toISOString();
-	} else {
-		Data.EndDate = "";
-	}
+	// if (Data.StartDate instanceof Date) {
+	// 	Data.StartDate = Data.StartDate.toISOString();
+	// } else {
+	// 	Data.StartDate = "";
+	// }
+	// if (Data.EndDate instanceof Date) {
+	// 	Data.EndDate = Data.EndDate.toISOString();
+	// } else {
+	// 	Data.EndDate = "";
+	// }
 		Data.SessionId= SessionId 
 
 	
 
 	const url = baseURL + '/Voyage/SaveVoyage?'; 
 	FormValid=true 
+	console.log(Data)
 	let data = await SuperFetch(url, Data, FormValid)
 
-	callback(data);
+	// callback(data);
 	window.location.href = '/Voyages';
 }
