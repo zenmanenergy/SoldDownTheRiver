d6ef95c8f57fdc930e28a49643163e940874212b
Steve Nelson <snelson@zenmanenergy.org>
2023-10-23 20:04:32 -0400

    added voyages

--- Client/src/routes/Voyages/+page.svelte
+++ Client/src/routes/Voyages/+page.svelte
@@ -1,53 +1,64 @@
 <style>
-    @import '/static/FormPages.css';
-  </style>
-  
-  <script>
-    import moment from 'moment';
-    import { onMount } from 'svelte';
-    import {handleGetVoyages} from './handleGetVoyages.js';
-    import { Session } from "../Session.js";
-  
-    export let voyages = [];
-    let isLoading = true;
-    async function setVoyages(data) {
-      voyages = data;
-    } 
-  
-    onMount(async () => {
-      await Session.handleSession();
-      await Promise.all([
+		@import '/static/FormPages.css';
+	</style>
+	
+	<script>
+		import moment from 'moment';
+		import { onMount } from 'svelte';
+		import {handleGetVoyages} from './handleGetVoyages.js';
+		import { Session } from "../Session.js";
+	
+		export let Voyages = [];
+		let isLoading = true;
+		async function setVoyages(data) {
+			Voyages = data;
+		} 
+	
+		onMount(async () => {
+			await Session.handleSession();
+			await Promise.all([
 				handleGetVoyages(Session.SessionId, setVoyages),
 
 			]);
-      isLoading = false;
-    });
-  
-    function addVoyage() {
-      window.location.href = '/Voyage?VoyageId=';
-    }
-  </script>
-  
-  <div class="section">
-    {#if isLoading}
-      <div class="loading-screen">
-        <div class="spinner"></div>
-      </div>
-    {:else}
-      <a href="/AdminMenu">Back to Menu</a>
-      <div class="ActionBox">
-        <h3 class="title is-2">List of Voyages</h3>
-        <ul>
-          {#each voyages as voyage}
-            <li>
-              <a href={`/Voyage?VoyageId=${voyage.VoyageId}`}>
-                {voyage.VoyageName}
-              </a>
-            </li>
-          {/each}
-        </ul>
-        <button on:click={addVoyage}>Add Voyage</button>
-      </div>
-    {/if}
-  </div>
-  
+			isLoading = false;
+		});
+	
+		function addVoyage() {
+			window.location.href = '/Voyage?VoyageId=';
+		}
+	</script>
+	
+	<div class="section">
+		{#if isLoading}
+			<div class="loading-screen">
+				<div class="spinner"></div>
+			</div>
+		{:else}
+			<a href="/AdminMenu">Back to Menu</a>
+			<div class="ActionBox">
+				<h3 class="title is-2">List of Voyages</h3>
+				<table class="ClickableTable" width=100%>
+					<thead>
+						<tr>
+							<th>Ship</th>
+							<th>From</th>
+							<th>To</th>
+						</tr>
+					</thead>
+					<tbody>
+						{#each Voyages as Voyage}
+							<tr on:click={() => window.location.href = `/Voyage?VoyageId=${Voyage.VoyageId}`}>
+								<td>{Voyage.ShipName}</td>
+								<td>{Voyage.StartCity} {Voyage.StartState} {moment(Voyage.StartDate).format('MMMM D, YYYY')}</td>
+								<td>{Voyage.EndCity} {Voyage.EndState} {moment(Voyage.EndDate).format('MMMM D, YYYY')}</td>
+							</tr>
+						{/each}
+
+						
+					</tbody>
+				</table>
+				<button on:click={addVoyage}>Add Voyage</button>
+			</div>
+		{/if}
+	</div>
+	
