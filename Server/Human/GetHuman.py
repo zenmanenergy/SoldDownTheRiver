2a9cff37f81ef68fa1c871d8d79ba4e1b16d5248
Steve Nelson <snelson@zenmanenergy.org>
2023-10-23 20:05:53 -0400

    removed roles

--- Server/Human/GetHuman.py
+++ Server/Human/GetHuman.py
@@ -7,13 +7,13 @@ def get_human(HumanId):
 	# Connect to the database
 	cursor, connection = Database.ConnectToDatabase()
 
-	# Construct the SQL query
-	query = "SELECT * , (select max(dateAdded) from History where History.KeyValue=Humans.HumanId and History.TableName='Humans' and History.KeyName='HumanId') LastModified"
-	query +=" FROM Humans left join HumanRoles on Humans.HumanId=HumanRoles.HumanId WHERE Humans.HumanId = %s"
-	values = (HumanId,)
-	print(query % values)
-	# Execute the query and get the results
-	cursor.execute(query, values)
+	# Construct the SQL sql
+	sql = f"SELECT * , (select max(dateAdded) from History where History.KeyValue=Humans.HumanId and History.TableName='Humans' and History.KeyName='HumanId') LastModified"
+	sql += f" FROM Humans WHERE Humans.HumanId = '{HumanId}'"
+	
+	print(sql)
+	# Execute the sql and get the results
+	cursor.execute(sql)
 	result = cursor.fetchone()
 	if not result:
 		result={}
