266de00698a56fe76517d09e7efec08f3b672364
Steve Nelson <snelson@zenmanenergy.org>
2023-10-23 20:03:26 -0400

    made the rows clickable

--- Client/src/routes/Roles/+page.svelte
+++ Client/src/routes/Roles/+page.svelte
@@ -1,36 +1,35 @@
 <style>
-@import '/static/FormPages.css';
-tbody tr:hover {
-	background-color: #444444; /* or any other color you like */
-	cursor: pointer;
-}
-</style>
+	@import '/static/FormPages.css';
+
 	
+</style>
+
 <script>
-import moment from 'moment';
-import { onMount } from 'svelte';
-import {handleGetRoles} from './handleGetRoles.js';
+	import moment from 'moment';
+	import { onMount } from 'svelte';
+	import {handleGetRoles} from './handleGetRoles.js';
 	import {Session} from "../Session.js";
 
-export let Roles = [];
-let isLoading = true;
-async function setRoles(data) {
-	
-	Roles=data;
-}
-onMount(async () => {
-		await Session.handleSession();
-	await Promise.all([
-	await handleGetRoles(Session.SessionId,setRoles)
-	]);
-	isLoading = false;
-});
+	export let Roles = [];
+	let isLoading = true;
 
-function addRole() {
-	window.location.href = '/Role?RoleId=';
-}
+	async function setRoles(data) {
+		Roles = data;
+	}
+
+	onMount(async () => {
+		await Session.handleSession();
+		await Promise.all([
+			await handleGetRoles(Session.SessionId, setRoles)
+		]);
+		isLoading = false;
+	});
+
+	function addRole() {
+		window.location.href = '/Role?RoleId=';
+	}
 </script>
-	
+
 {#if isLoading}
 	<div class="loading-screen">
 		<div class="spinner"></div>
@@ -40,24 +39,23 @@ function addRole() {
 		<a href="/AdminMenu">Back to Menu</a>
 		<div class="ActionBox">
 			<h3 class="title is-2">List of Roles</h3>
-			<table width=100%>
-			<thead>
-				<tr>
-				<th>Role</th>
-				<th>Last Modified</th>
-				</tr>
-			</thead>
-			<tbody>
-				{#each Roles as role}
-				<tr style="cursor: pointer;" on:click={location.href=`/Role?RoleId=${role.RoleId}`}>
-					<td>{role.Role}</td>
-					<td>{moment.utc(role.LastModified).local().fromNow()}</td>
-				</tr>
-				{/each}
-			</tbody>
+			<table class="ClickableTable" width=100%>
+				<thead>
+					<tr>
+						<th>Role</th>
+						<th>Last Modified</th>
+					</tr>
+				</thead>
+				<tbody>
+					{#each Roles as role (role.RoleId)}
+						<tr on:click={() => location.href=`/Role?RoleId=${role.RoleId}`}>
+							<td>{role.Role}</td>
+							<td>{moment.utc(role.LastModified).local().fromNow()}</td>
+						</tr>
+					{/each}
+				</tbody>
 			</table>
 			<button on:click={addRole}>Add Role</button>
 		</div>
 	</div>
-	
 {/if}
