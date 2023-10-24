7d040720b05d6d19233b97d9506ee70ff152beb3
Steve Nelson <snelson@zenmanenergy.org>
2023-10-23 20:07:43 -0400

    added voyage locations

--- Server/Voyages/GetVoyages.py
+++ Server/Voyages/GetVoyages.py
@@ -5,11 +5,16 @@ def get_voyages():
 	cursor, connection = Database.ConnectToDatabase()
 
 	# Construct the SQL query for voyages
-	query = "SELECT *, (SELECT MAX(dateAdded) FROM History WHERE History.KeyValue=Voyages.VoyageId AND History.TableName='Voyages' AND History.KeyName='VoyageId') AS LastModified"
-	query += " FROM Voyages ORDER BY StartDate"
+	
+	sql = f"SELECT voyages.VoyageId, voyages.StartDate, voyages.EndDate, ships.ShipName,"
+	sql += f" startLocation.City StartCity, startLocation.State StartState, EndLocation.City EndCity, EndLocation.State EndState,"
+	sql += f" (select max(dateAdded) from History where History.KeyValue=voyages.VoyageId and History.TableName='Voyages' and History.KeyName='VoyageId') LastModified "
+	sql += f" FROM voyages join ships on Voyages.shipId=ships.ShipId "
+	sql += f" left join locations startLocation on startLocation.LocationId=voyages.StartLocationId"
+	sql += f" left join locations endLocation on endLocation.LocationId=voyages.EndLocationId"
 
 	# Execute the query and get the results
-	cursor.execute(query)
+	cursor.execute(sql)
 	result = cursor.fetchall()
 	if not result:
 		result = []
