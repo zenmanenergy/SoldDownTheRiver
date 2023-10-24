4b952bf96c05ceac6fe0bddbd68207381d0aa6e9
Steve Nelson <snelson@zenmanenergy.org>
2023-10-23 20:06:21 -0400

    added gethuman andsavehumanrole

--- Server/Role/Role.py
+++ Server/Role/Role.py
@@ -7,6 +7,9 @@ from .SaveRole import save_role
 from .DeleteRole import delete_role
 from .GetRole import get_role
 from .GetRoleHumans import get_roleHumans
+from .GetHumans import get_Humans
+from .SaveHumanRole import save_HumanRole
+
 from Lib import History
 
 blueprint = Blueprint('Role', __name__)
@@ -77,6 +80,41 @@ def GetRoleHumans():
 		
 		# Call the get_role function from GetRoles.py
 		result = get_roleHumans(RoleId)
+
+		return result
+	except Exception as e:
+		return Debugger(e)
+
+
+@blueprint.route("/Role/GetHumans", methods=['GET'])
+@cross_origin()
+def GetHumans():
+	try:
+		# Get the role data from the request
+		role_data = request.args.to_dict()
+		RoleId = role_data.get('RoleId')
+		
+		Query = role_data.get('Query')
+		
+		
+		# Call the get_role function from GetRoles.py
+		result = get_Humans(RoleId,Query)
+
+		return result
+	except Exception as e:
+		return Debugger(e)
+
+@blueprint.route("/Role/SaveHumanRole", methods=['GET'])
+@cross_origin()
+def SaveHumanRole():
+	try:
+		role_data = request.args.to_dict()
+		RoleId = role_data.get('RoleId')
+		HumanId = role_data.get('HumanId')
+		
+		
+		# Call the get_role function from GetRoles.py
+		result = save_HumanRole(HumanId,RoleId)
 
 		return result
 	except Exception as e:
