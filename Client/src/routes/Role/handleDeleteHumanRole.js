e7e9a14167279a5b6a8a136ab935a9a8449d5733
Steve Nelson <snelson@zenmanenergy.org>
2023-10-26 16:06:29 -0400

    initial commit

---
+++ Client/src/routes/Role/handleDeleteHumanRole.js
@@ -0,0 +1,17 @@
+// src/routes/Roles/handleDelete.js
+import { baseURL } from '../Settings';
+import { SuperFetch } from '../SuperFetch';
+
+export async function handleDeleteHumanRole(SessionId,RoleId,HumanId) {
+
+	const Data = {
+		SessionId:SessionId,
+		RoleId:RoleId,
+		HumanId:HumanId
+	};
+	const url = baseURL + '/Role/DeleteHumanRole?'; 
+	const FormValid=true
+	let data = await SuperFetch(url, Data, FormValid)
+
+	window.location.href = '/Role?RoleId='+RoleId;
+}
---
+++ Server/Role/DeleteHumanRole.py
@@ -0,0 +1,15 @@
+from Lib import Database
+
+def delete_HumanRole(HumanId,RoleId):
+	# Delet
+	# e the specified row from the Roles table
+	sql = f"DELETE FROM HumanRoles WHERE RoleId = '{RoleId}' and HumanId='{HumanId}'"
+	cursor, connection = Database.ConnectToDatabase()
+	print(sql)
+	cursor.execute(sql)
+	connection.commit()
+
+	
+	connection.close()
+
+	return {'success': True,'message': f'Role "{RoleId}" deleted successfully.'}
