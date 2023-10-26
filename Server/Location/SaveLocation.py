8544901e8c2b6b54961dcdf394cae424f6e38118
Steve Nelson <snelson@zenmanenergy.org>
2023-10-26 16:08:39 -0400

    changed to an f string

--- Server/Location/SaveLocation.py
+++ Server/Location/SaveLocation.py
@@ -5,35 +5,25 @@ def save_location(LocationId, City, State, Country, Latitude, Longitude):
 	# Connect to the database
 	cursor, connection = Database.ConnectToDatabase()
 
-	
-	try:
-		# Check if the LocationId is present
-		if LocationId:
-			# If the LocationId is present, update the existing location
-			query = "UPDATE Locations SET City = %s, State = %s, Country = %s, Latitude = %s, Longitude = %s WHERE LocationId = %s"
-			values = (City, State, Country, Latitude, Longitude, LocationId)
-		else:
-			# If the LocationId is not present, create a new location
-			LocationId = "LOC"+str(uuid.uuid4())
-			query = "INSERT INTO Locations (LocationId, City, State, Country, Latitude, Longitude) VALUES (%s, %s, %s, %s, %s, %s)"
-			
-			query +=" ON DUPLICATE KEY UPDATE City=values(City),State=values(State),Country=values(Country),Latitude=values(Latitude),Longitude=values(Longitude) "
-
-			values = (LocationId, City, State, Country, float(Latitude or 0.0), float(Longitude or 0.0))
-
-		response = {'success': True, 'LocationId': LocationId}
-		# Execute the query and commit the changes
-		cursor.execute(query, values)
-		connection.commit()
+	sql=""
+	# Check if the LocationId is present
+	if LocationId:
+		# If the LocationId is present, update the existing location
+		sql = f"UPDATE Locations SET City = '{City}', State = '{State}', Country = '{Country}', Latitude = '{float(Latitude or 0.0)}', Longitude = '{float(Longitude or 0.0)}' WHERE LocationId = '{LocationId}'"
+		
+	else:
+		# If the LocationId is not present, create a new location
+		LocationId = "LOC"+str(uuid.uuid4())
+		sql =f"INSERT INTO Locations (LocationId, City, State, Country, Latitude, Longitude) VALUES ('{LocationId}', '{City}', '{State}', '{Country}', '{float(Latitude or 0.0)}', '{float(Longitude or 0.0)}')"
+		
+		sql +=" ON DUPLICATE KEY UPDATE City=values(City),State=values(State),Country=values(Country),Latitude=values(Latitude),Longitude=values(Longitude) "
 
 
-	except pymysql.err.IntegrityError as e:
-		# If a duplicate entry error occurred, modify the response
-		response = {'success': False, 'error': str(e), 'LocationId': LocationId}
-
-	finally:
-		# Close the database connection
-		connection.close()
+	# Execute the sql and commit the changes
+	cursor.execute(sql)
+	connection.commit()
+	connection.close()
+	response = {'success': True, 'LocationId': LocationId}
 
 	print(response)
 	# Return the response
