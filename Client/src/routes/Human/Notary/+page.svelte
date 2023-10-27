7a6fc4186be1993c841e66d0ecfeb665c20738f7
Steve Nelson <snelson@zenmanenergy.org>
2023-10-27 17:40:22 -0400

    removed ships

--- Client/src/routes/Human/Notary/+page.svelte
+++ Client/src/routes/Human/Notary/+page.svelte
@@ -11,7 +11,6 @@
 	import { handleGetRoles } from '../handleGetRoles.js';
 	import { handleGetHumanLocations } from '../handleGetHumanLocations.js';
 	import { handleGetHumanRoles } from '../handleGetHumanRoles.js';
-	import { handleGetShips } from './handleGetShips.js';
 	// import { handleGetAKA } from '../handleGetAKA.js';
 	// import { handleSaveAkaName } from '../handleSaveAkaName.js';
 	// import { handleDeleteAkaName } from '../handleDeleteAkaName.js';
@@ -27,7 +26,6 @@
 	let Roles=[];
 	let Locations=[];
 	let HumanRoles=[];
-	let Ships=[];
 	// let AkaNames = [];
 
 	async function setHuman(data) {
@@ -61,7 +59,6 @@
 			handleGetRoles(Session.SessionId,setRoles),
 			handleGetHumanLocations(Session.SessionId,HumanId,setLocations),
 			handleGetHumanRoles(Session.SessionId,HumanId,setHumanRoles),
-			handleGetShips(Session.SessionId,HumanId, setShips),
 			
 		]);
 	 
@@ -76,9 +73,9 @@
 	</div>
 {:else}
 <div class="section">
-	<a href="/Role?RoleId=ShipOwner">Back to Ship Agents</a>
+	<a href="/Role?RoleId=Notary">Back to Notaries</a>
 	<div class="ActionBox">
-		<h3 class="title is-2">Edit Ship Agent</h3>
+		<h3 class="title is-2">Edit Notary</h3>
 	<form>
 		<div class="field">
 			<label class="label" for="FirstName">First Name:</label>
@@ -120,8 +117,8 @@
 	{#if Human.LastModified}
 		<small>Last Modified: {moment.utc(Human.LastModified).local().fromNow()}</small>
 	{/if}
-
-	<br/>
+	{#if HumanId}
+		<br/>
 		<div class="ActionBox">
 			<h3 class="title is-2">Roles</h3>
 			<form>
@@ -143,54 +140,31 @@
 			
 		</div>
 		<br/>
-	<div class="ActionBox">
-		<h3 class="title is-2">Ships</h3>
-		<form>
-		
-		<table class="ClickableTable" width=100%>
-			<thead>
-				<tr>
-					<th>Name</th>
-					<th>Type</th>
-					<th>Home Port</th>
-				</tr>
-			</thead>
-			<tbody>
-				{#each Ships as Ship}
-					<tr on:click={() => location.href=`/Ship/?ShipId=${Ship.ShipId}`}>
-						<td>{Ship.ShipName}</td>
-						<td>{Ship.ShipType}</td>
-						<td>{Ship.City}, {Ship.State}</td>
+	
+		<br/>
+		<div class="ActionBox">
+			<h3 class="title is-2">Timeline of known locations</h3>
+			<form>
+			
+			<table class="ClickableTable" width=100%>
+				<thead>
+					<tr>
+						<th>Location</th>
+						<th>When</th>
 					</tr>
-				{/each}
-				
-			</tbody>
-		</table>
-		
-	</div>
-	<br/>
-	<div class="ActionBox">
-		<h3 class="title is-2">Timeline of known locations</h3>
-		<form>
-		
-		<table class="ClickableTable" width=100%>
-			<thead>
-				<tr>
-					<th>Location</th>
-					<th>When</th>
-				</tr>
-			</thead>
-			<tbody>
-				{#each Locations as Location}
-					<tr style="cursor: pointer;">
-						<td>{Location.City}, {Location.State}</td>
-						<td>{moment(Location.DateCirca).format('MM/DD/YYYY')}</td>
-					</tr>
-				{/each}
-			</tbody>
-		</table>
-		
-	</div>
+				</thead>
+				<tbody>
+					{#each Locations as Location}
+						<tr style="cursor: pointer;">
+							<td>{Location.City}, {Location.State}</td>
+							<td>{moment(Location.DateCirca).format('MM/DD/YYYY')}</td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+			
+		</div>
+	{/if}
 </div>
 </div>
 	{/if}
