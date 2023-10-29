0728b87edc0f3525d3e2c7c220bc1cbe32afc126
Steve Nelson <snelson@zenmanenergy.org>
2023-10-28 23:37:47 -0400

    initial create

---
+++ Server/Voyage/EnslavedPerson/SaveEnslavedPerson.py
@@ -0,0 +1,29 @@
+import uuid
+from Lib import Database
+import datetime
+
+def save_EnslavedPerson(HumanId, RoleId, FirstName, MiddleName, LastName):
+	
+	# Connect to the database
+	cursor, connection = Database.ConnectToDatabase()
+	
+
+
+	
+	query = "INSERT INTO Humans (VoyageId, HumanId, RoleId, SellingSlaveTraderHumanId,BuyingSlaveTraderHumanId,ShippingAgentHumanId,CollectingAgentHumanId,Notes)"
+	query += f" VALUES ('{VoyageId}', '{HumanId}','{RoleId}','{SellingSlaveTraderHumanId}', '{BuyingSlaveTraderHumanId}''{ShippingAgentHumanId}', '{CollectingAgentHumanId}','{Notes}')"
+	query +=" ON DUPLICATE KEY UPDATE VoyageId=values(VoyageId),HumanId=values(HumanId) ,RoleId=values(RoleId), SellingSlaveTraderHumanId=values(SellingSlaveTraderHumanId),BuyingSlaveTraderHumanId=values(BuyingSlaveTraderHumanId),ShippingAgentHumanId=values(ShippingAgentHumanId),CollectingAgentHumanId=values(CollectingAgentHumanId),Notes=values(Notes)"
+		
+
+
+	print(query )
+
+	# Execute the query and commit the changes
+	cursor.execute(query)
+	connection.commit()
+
+	# Close the database connection 
+	connection.close()
+
+	# Return the VoyageId as a JSON response
+	return {'success': True, 'VoyageId:HumanId': VoyageId+":"+HumanId}
